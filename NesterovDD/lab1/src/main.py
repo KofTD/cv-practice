05a7c6ae066f0ecc1fc7a228137c8a6222df86ed
KofTD <KOfTD@yandex.ru>
2025-10-25 11:38:21 +0300

    Add arguments parsing

--- NesterovDD/lab1/src/main.py
+++ NesterovDD/lab1/src/main.py
@@ -1,37 +1,123 @@
 import cv2
 import filters
+import argparse
 
 from labTypings import RGB, Rectangle
 
 
-def main():
-    nInputImages = 4
-    nFigureFrames = 1
-    for i in range(nInputImages):
-        inputImagePath = f"input/input{i + 1}.jpg"
+def main(args: argparse.Namespace):
+    imagePath = args.image
 
-        inputImage = cv2.imread(inputImagePath)
-        if inputImage is None:
-            raise ValueError(f"There is no input{i + 1}.jpg in input folder")
+    image = cv2.imread(imagePath)
 
-        result = filters.resize(inputImage, 2000, 1000)
-        _ = cv2.imwrite(f"output/output{i + 1}_resize.jpg", result)
-        result = filters.sepia(inputImage)
-        _ = cv2.imwrite(f"output/output{i + 1}_sepia.jpg", result)
-        result = filters.addVignette(inputImage, 0.8, 0.6)
-        _ = cv2.imwrite(f"output/output{i + 1}_vignete.jpg", result)
-        result = filters.pixelate(inputImage, Rectangle(100, 100, 1200, 300), 16)
-        _ = cv2.imwrite(f"output/output{i + 1}_pixelate.jpg", result)
-        result = filters.addRectFrame(inputImage, RGB(255, 0, 0), 10)
-        _ = cv2.imwrite(f"output/output{i + 1}_rectFrame.jpg", result)
-        for j in range(nFigureFrames):
-            result = filters.addFigureFrame(inputImage, j + 1, 30)
-            _ = cv2.imwrite(f"output/output{i + 1}_figureFrame{j + 1}.jpg", result)
-        result = filters.addGlare(inputImage, 0.86)
-        _ = cv2.imwrite(f"output/output{i + 1}_glare.jpg", result)
-        result = filters.addWatercolor(inputImage)
-        _ = cv2.imwrite(f"output/output{i + 1}_watercolor.jpg", result)
+    if image is None:
+        raise ValueError(f"There is no image: {imagePath}")
+
+    if args.filterType == "resize":
+        height: int | None = args.height
+        width: int | None = args.width
+
+        if height is None:
+            height = int(image.shape[0])
+        if width is None:
+            width = int(image.shape[1])
+        filteredImage = filters.resize(image, width, height)
+    elif args.filterType == "sepia":
+        filteredImage = filters.sepia(image)
+    elif args.filterType == "vignette":
+        opacity: float | None = args.opacity
+
+        if opacity is None:
+            filteredImage = filters.addVignette(image, args.radius)
+        else:
+            filteredImage = filters.addVignette(image, args.radius, opacity)
+    elif args.filterType == "pixelate":
+        rect = Rectangle(*args.region)
+        filteredImage = filters.pixelate(image, rect, args.blockSize)
+    elif args.filterType == "rectFrame":
+        borderColor = RGB(*args.color)
+        filteredImage = filters.addRectFrame(image, borderColor, args.width)
+    elif args.filterType == "figureFrame":
+        whiteThreshold: float | None = args.whiteThreshold
+
+        if whiteThreshold is None:
+            filteredImage = filters.addFigureFrame(
+                image,
+                args.frameIndex,
+            )
+        else:
+            filteredImage = filters.addFigureFrame(
+                image, args.frameIndex, whiteThreshold
+            )
+    elif args.filterType == "glare":
+        opacity: float | None = args.opacity
+
+        if opacity is None:
+            filteredImage = filters.addGlare(image)
+        else:
+            filteredImage = filters.addGlare(image, opacity)
+    elif args.filterType == "watercolor":
+        opacity: float | None = args.opacity
+
+        if opacity is None:
+            filteredImage = filters.addWatercolor(image)
+        else:
+            filteredImage = filters.addWatercolor(image, opacity)
+
+    cv2.imshow("Original", image)
+    cv2.imshow("Filtered", filteredImage)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    argParser = argparse.ArgumentParser(prog="cvFilter")
+    argParser.add_argument("--image", type=str, required=True, help="Path to image")
+
+    subparsers = argParser.add_subparsers(
+        dest="filterType", title="Filters", help="A filter applied to the image"
+    )
+
+    resizeParser = subparsers.add_parser("resize")
+    resizeParser.add_argument(
+        "--height", type=int, help="If not specified, original height will be used"
+    )
+    resizeParser.add_argument(
+        "--width", type=int, help="If not specified, original height will be used"
+    )
+
+    sepiaParser = subparsers.add_parser("sepia")
+    vignetteParser = subparsers.add_parser("vignette")
+    vignetteParser.add_argument(
+        "--radius", type=float, required=True, help="Value from 0 to 1"
+    )
+    vignetteParser.add_argument("--opacity", type=float)
+
+    pixelateParser = subparsers.add_parser("pixelate")
+    pixelateParser.add_argument(
+        "--region",
+        type=int,
+        help="Coordiantes of top left cornern, width and height",
+        required=True,
+        nargs=4,
+    )
+    pixelateParser.add_argument("--blockSize", type=int, required=True)
+
+    rectFrameParser = subparsers.add_parser("rectFrame")
+    rectFrameParser.add_argument(
+        "--color", type=int, help="RGB", required=True, nargs=3
+    )
+    rectFrameParser.add_argument("--width", type=int, required=True)
+
+    figureFrameParser = subparsers.add_parser("figureFrame")
+    figureFrameParser.add_argument("--frameIndex", type=int, required=True)
+    figureFrameParser.add_argument("--whiteThreshold", type=float)
+
+    glareParser = subparsers.add_parser("glare")
+    glareParser.add_argument("--opacity", type=float)
+
+    watercolorParser = subparsers.add_parser("watercolor")
+    watercolorParser.add_argument("--opacity", type=float)
+
+    main(argParser.parse_args())
